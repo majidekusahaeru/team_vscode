39d82314c1f9321bccd9f97060046d45e5414c56
fujita manato <m.fujita.sys20@morijyobi.ac.jp>
2021-03-30 12:20:51 +0900

    青終わりました。

--- app.py
+++ app.py
@@ -5,13 +5,18 @@ app = Flask(__name__)
 @app.route("/")
 def top():
     return render_template("index.html")
+
+@app.route("/enkeisan")
+def enkeisan():
+    return render_template("keisan1.html")
+
 @app.route("/keisan")
 def keisan():
     hankei= int(request.args.get("hankei"))
     p = 314
     syu = hankei * 2 * p /100
     men = hankei * hankei * p /100
-    return render_template("menu.html",syu = syu,men = men)
+    return render_template("hantei.html",syu = syu,men = men)
 
 if __name__ == "__main__":
 
